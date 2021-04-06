c49cf1d6e15056bd29a9ed6dc1b867cb05bf27b0
Kim MinGuk <alsrnr315@gmail.com>
2021-04-06 11:51:05 +0900

    Init PlaceSearchVC

---
+++ ReDawaga/ViewController/PlaceSearchViewController.swift
@@ -0,0 +1,130 @@
+//
+//  PlaceSearchViewController.swift
+//  ReDawaga
+//
+//  Created by 김민국 on 2021/04/05.
+//
+
+import UIKit
+import ReSwift
+
+class PlaceSearchViewController: UIViewController {
+
+    // MARK: - UI Initialization
+    
+    private var searchView: PlaceSearchView!
+    private var placeTableView: UITableView!
+    
+    
+    // MARK: - Property
+    
+    private var placeList: [PlaceEntity] = [] {
+        didSet {
+            placeTableView.reloadData()
+        }
+    }
+    
+    private var placeAddress: String = ""
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        appStore.subscribe(self)
+        self.view.backgroundColor = .white
+        
+        navigationController?.navigationBar.isHidden = false
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.topItem?.title = ""
+        navigationItem.title = NavigationTitle.DestinationAddress.localized()
+    }
+             
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        appStore.unsubscribe(self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupSearchView()
+        setupPlaceTableView()
+    }
+    
+    
+    // MARK: - Function
+
+    func setupSearchField(address: String) {
+        self.placeAddress = address
+    }
+    
+    private func setupSearchView() {
+        searchView = PlaceSearchView()
+        view.addSubview(searchView)
+        
+        searchView.configureItem(address: self.placeAddress)
+        
+        searchView.searchButtonAction = { address in
+            PlaceSearchActionCreator.fetchPlaceList(address: address ?? "")
+            
+        }
+        
+        searchView.snp.makeConstraints { (make) in
+            make.left.right.equalToSuperview()
+            make.top.equalToSuperview().offset(100)
+            make.height.equalTo(PlaceSearchView.VIEW_HEIGHT)
+        }
+    }
+    
+    private func setupPlaceTableView() {
+        placeTableView = UITableView()
+        view.addSubview(placeTableView)
+        
+        placeTableView.register(PlaceSearchTableViewCell.self, forCellReuseIdentifier: PlaceSearchTableViewCell.self.identifier)
+        placeTableView.delegate = self
+        placeTableView.dataSource = self
+        placeTableView.estimatedRowHeight = PlaceSearchTableViewCell.CELL_HEIGHT
+        
+        placeTableView.snp.makeConstraints { (make) in
+            make.left.right.bottom.equalToSuperview()
+            make.top.equalTo(searchView.snp.bottom)
+        }
+    }
+}
+
+extension PlaceSearchViewController: StoreSubscriber  {
+    
+    func newState(state: AppState) {
+        // 이런식으로 추가
+        // let isPlaceEmpty = emptyErrorView.isHidden ? True : False
+        
+        self.placeList = state.placeSearchState.placeList
+    }
+}
+
+extension PlaceSearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let place = placeList[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: PlaceSearchTableViewCell.self.identifier) as! PlaceSearchTableViewCell
+        cell.configurePlace(place: place)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return PlaceSearchTableViewCell.CELL_HEIGHT
+    }
+}
