993702319f3266d460db271780f0de1ea259fd63
Kim MinGuk <alsrnr315@gmail.com>
2021-04-14 00:11:49 +0900

    Add UICollectionViewCell Extension

---
+++ ReDawaga/Extension/UICollectionView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionView+Extension.swift
+//  ReDawaga
+//
+//  Created by 김민국 on 2021/04/08.
+//
+
+import UIKit
+import Foundation
+
+extension UICollectionViewCell {
+
+    static var identifier: String {
+        return className
+    }
+}
