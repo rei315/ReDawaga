b649b54c816fe4647c3a4a8031242bd5f067bf64
Kim MinGuk <alsrnr315@gmail.com>
2021-04-14 03:17:25 +0900

    Update Constant

--- ReDawaga/Constants/Constants.swift
+++ ReDawaga/Constants/Constants.swift
@@ -1,12 +1,1 @@
-//
-//  Constants.swift
-//  ReDawaga
-//
-//  Created by 김민국 on 2021/03/29.
-//
-
-import Foundation
-
-struct Constants {
-    static let GOOGLE_API_KEY = ""
-}
+struct Constants { static let GOOGLE_API_KEY = "" }
