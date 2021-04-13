25b7f7ff465dfa2f79867efaa7295ead39a8451f
Kim MinGuk <alsrnr315@gmail.com>
2021-04-14 00:11:26 +0900

    Edit MarkRealm Model (Add some features)

--- ReDawaga/Model/MarkRealm.swift
+++ ReDawaga/Model/MarkRealm.swift
@@ -25,4 +25,67 @@ class MarkRealm {
             }
         }
     }
+    
+    static func saveMarkRealm(mark: MarkRealmEntity) -> Promise<Void> {
+        return Promise { seal in
+            do {
+                let realm = try Realm()
+                
+                try realm.write {
+                    realm.add(mark)
+                    seal.fulfill(())
+                }
+            }
+            catch {
+                seal.reject(error)
+            }
+        }
+    }
+    
+    static func editMarkRealm(identity: String, name: String = "", address: String = "", iconImage: String = "", latitude: Double = 0, longitude: Double = 0) -> Promise<Bool> {
+        return Promise { seal in
+            let realm = try Realm()
+            guard let data = realm.object(ofType: MarkRealmEntity.self, forPrimaryKey: identity) else {
+                seal.fulfill(false)
+                return
+            }
+            
+            do {
+                try realm.write {
+                    let newData = MarkRealmEntity(
+                        identity: data.identity,
+                        name: name.isEmpty ? data.name : name,
+                        latitude: latitude.isZero ? data.latitude : latitude,
+                        longitude: longitude.isZero ? data.longitude : longitude,
+                        address: address.isEmpty ? data.address : address,
+                        iconImageUrl: iconImage.isEmpty ? data.iconImageUrl : iconImage
+                    )
+                    realm.add(newData, update: .modified)
+                    seal.fulfill(true)
+                }
+            }
+            catch {
+                seal.reject(error)
+            }
+        }
+    }
+    
+    static func removeMarkRealm(identity: String) -> Promise<Bool> {
+        return Promise { seal in
+            let realm = try Realm()
+            guard let data = realm.object(ofType: MarkRealmEntity.self, forPrimaryKey: identity) else {
+                seal.fulfill(false)
+                return
+            }
+            do {
+                try realm.write {
+                    realm.delete(data)
+                    seal.fulfill(true)
+                }
+            }
+            catch {
+                seal.reject(error)
+            }
+        }
+    }
 }
