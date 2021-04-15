8565b1d9e5b7d0267908756f0ee6e2af17642096
Kim MinGuk <alsrnr315@gmail.com>
2021-04-15 12:01:39 +0900

    Add TutorialViewController

---
+++ ReDawaga/ViewController/TutorialViewController.swift
@@ -0,0 +1,194 @@
+//
+//  TutorialViewController.swift
+//  ReDawaga
+//
+//  Created by 김민국 on 2021/04/15.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    // MARK: - UI Initialization
+    
+    private lazy var tutorialView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.backgroundColor = .white
+        sv.isPagingEnabled = true
+        sv.showsHorizontalScrollIndicator = false
+        sv.delegate = self
+        return sv
+    }()
+    
+    private lazy var tutorialPage: UIPageControl = {
+        let pc = UIPageControl()
+        pc.pageIndicatorTintColor = UIColor.middleBlue
+        pc.currentPageIndicatorTintColor = UIColor.systemOrange
+        pc.translatesAutoresizingMaskIntoConstraints = false
+        return pc
+    }()
+    
+    private lazy var tutorialDoneButton: UIButton = {
+        let bt = UIButton()
+        bt.backgroundColor = UIColor.systemOrange
+        bt.layer.cornerRadius = 8
+        bt.tintColor = .white
+        bt.setTitle(AppString.TutorialDoneTitle.localized(), for: .normal)
+        bt.isHidden = true
+        bt.addTarget(self, action: #selector(onTutorialDoneButton), for: .touchUpInside)
+        return bt
+    }()
+    
+    
+    // MARK: - Property
+    
+    private var tutorialImages: [UIImage] = []
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTutorialView()
+    }
+    
+    
+    // MARK: - Function
+    
+    @objc private func onTutorialDoneButton() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+
+// MARK: - ScrollView Delegate
+extension TutorialViewController: UIScrollViewDelegate  {
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+        tutorialPage.currentPage = Int(pageNumber)
+        
+        let maxPage = tutorialImages.count - 1
+        
+        if tutorialPage.currentPage == maxPage && tutorialDoneButton.isHidden {
+            self.animateTutorialDoneButton(state: .Up)
+        }
+        else if tutorialPage.currentPage != maxPage && !tutorialDoneButton.isHidden {
+            self.animateTutorialDoneButton(state: .Down)
+        }
+    }
+}
+
+// MARK: - UI Setup
+extension TutorialViewController {
+    
+    private func setupTutorialView() {
+        view.addSubview(tutorialView)
+        view.addSubview(tutorialPage)
+        view.addSubview(tutorialDoneButton)
+        
+        switch Locale.current.languageCode {
+        case "en":
+            tutorialImages.append(UIImage(named: "tutorial_eng_1") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_2") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_3") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_4") ?? UIImage())
+        case "ko":
+            tutorialImages.append(UIImage(named: "tutorial_ko_1") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ko_2") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ko_3") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ko_4") ?? UIImage())
+        case "ja":
+            tutorialImages.append(UIImage(named: "tutorial_ja_1") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ja_2") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ja_3") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_ja_4") ?? UIImage())
+        case .none:
+            tutorialImages.append(UIImage(named: "tutorial_eng_1") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_2") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_3") ?? UIImage())
+            tutorialImages.append(UIImage(named: "tutorial_eng_4") ?? UIImage())
+            break
+        case .some(_):
+            break
+        }
+        
+        let pageWidth = self.view.frame.width
+        
+        tutorialView.contentSize = CGSize(width: CGFloat(tutorialImages.count) * self.view.frame.maxX, height: 0)
+        
+        tutorialView.snp.makeConstraints { (make) in
+            make.edges.equalToSuperview()
+        }
+        tutorialPage.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(70)
+        }
+        
+        tutorialImages.enumerated().forEach { index, image in
+            let backView = UIView()
+            let iv = UIImageView()
+            iv.image = image
+            iv.contentMode = .scaleAspectFit
+            backView.addSubview(iv)
+            
+            tutorialView.addSubview(backView)
+            
+            let offset = CGFloat(index) * pageWidth
+            
+            backView.snp.makeConstraints { (make) in
+                make.width.equalTo(view.frame.width)
+                make.height.equalTo(view.frame.height)
+                make.left.equalTo(tutorialView).offset(offset)
+                make.centerY.equalToSuperview()
+            }
+            
+            iv.snp.makeConstraints { (make) in
+                make.bottom.equalToSuperview()
+                make.top.equalTo(tutorialPage.snp.bottom).offset(10)
+                make.centerX.equalToSuperview()
+            }
+        }
+        
+        tutorialPage.numberOfPages = tutorialImages.count
+        
+        tutorialDoneButton.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(50)
+            make.width.equalToSuperview().multipliedBy(0.4)
+            make.height.equalTo(50)
+        }
+    }
+}
+
+
+// MARK: - DoneButton Animation
+extension TutorialViewController {
+    
+    enum TutorialDoneButtonAnimation { case Up, Down }
+    
+    private func animateTutorialDoneButton(state: TutorialDoneButtonAnimation) {
+        switch state {
+        case .Up:
+            tutorialDoneButton.isHidden = false
+            tutorialDoneButton.snp.updateConstraints { (make) in
+                make.bottom.equalToSuperview().offset(-100)
+            }
+            
+            UIView.animate(withDuration: 0.5) {
+                self.view.layoutIfNeeded()
+            }
+            
+        case .Down:
+            tutorialDoneButton.snp.updateConstraints { (make) in
+                make.bottom.equalToSuperview().offset(50)
+            }
+            self.tutorialDoneButton.isHidden = true
+            UIView.animate(withDuration: 0.5) {
+                self.view.layoutIfNeeded()
+            }
+        }
+    }
+}
