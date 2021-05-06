066528297494d705eb197c5966b833eac5060920
Kim MinGuk <alsrnr315@gmail.com>
2021-05-06 13:00:22 +0900

    Add NetworkConnectionError Localization

--- ReDawaga/Constants/App.swift
+++ ReDawaga/Constants/App.swift
@@ -56,6 +56,9 @@ extension AppString {
     static let TutorialDoneTitle = "TutorialDoneTitle"
     
     static let Enter = "Enter"
+    
+    static let NetworkConnectionErrorTitle = "NetworkConnectionErrorTitle"
+    static let NetworkConnectionErrorMessage = "NetworkConnectionErrorMessage"
 }
 
 enum Url {}
