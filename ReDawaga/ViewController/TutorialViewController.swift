d333ff398c5df780474307a980f6288f039369c3
Kim MinGuk <alsrnr315@gmail.com>
2021-05-06 13:14:48 +0900

    Update TutorialViewController ( Add some features )

--- ReDawaga/ViewController/TutorialViewController.swift
+++ ReDawaga/ViewController/TutorialViewController.swift
@@ -16,6 +16,8 @@ class TutorialViewController: UIViewController {
         sv.backgroundColor = .white
         sv.isPagingEnabled = true
         sv.showsHorizontalScrollIndicator = false
+        sv.showsVerticalScrollIndicator = false
+        sv.contentInsetAdjustmentBehavior = .never
         sv.delegate = self
         return sv
     }()
@@ -32,6 +34,7 @@ class TutorialViewController: UIViewController {
         let bt = UIButton()
         bt.backgroundColor = UIColor.systemOrange
         bt.layer.cornerRadius = 8
+        bt.clipsToBounds = true
         bt.tintColor = .white
         bt.setTitle(AppString.TutorialDoneTitle.localized(), for: .normal)
         bt.isHidden = true
@@ -45,7 +48,14 @@ class TutorialViewController: UIViewController {
     private var tutorialImages: [UIImage] = []
     
     
+    
     // MARK: - Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.setupNavigationController()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,9 +65,32 @@ class TutorialViewController: UIViewController {
     
     
     // MARK: - Function
+    private let tutorialDoneButtonHeight: CGFloat = 60
     
     @objc private func onTutorialDoneButton() {
-        self.dismiss(animated: true, completion: nil)
+        self.tutorialDoneButton.setTitle("", for: .normal)
+        
+        tutorialDoneButton.snp.updateConstraints { make in
+            make.width.height.equalTo(tutorialDoneButtonHeight)
+        }
+        
+        tutorialDoneButton.layer.cornerRadius = tutorialDoneButtonHeight/2
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut) {
+            self.view.layoutIfNeeded()
+        } completion: { [weak self] _ in
+            let largeConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .regular, scale: .medium)
+            self?.tutorialDoneButton.setImage(UIImage(systemName: "arrow.clockwise.circle", withConfiguration: largeConfig), for: .normal)
+            self?.tutorialDoneButton.rotate()
+        }
+        
+        UserDefaults.standard.setValue(true, forKey: "LaunchedBefore")
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+        DispatchQueue.main.asyncAfter(deadline: .now()+1.5, execute: {
+            appDelegate.setupMainView()
+        })
     }
 }
 
@@ -100,30 +133,19 @@ extension TutorialViewController {
             make.top.equalToSuperview().offset(70)
         }
         
-        switch Locale.current.languageCode {
-        case "en":
-            tutorialImages.append(UIImage(named: "tutorial_eng_1") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_2") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_3") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_4") ?? UIImage())
-        case "ko":
-            tutorialImages.append(UIImage(named: "tutorial_ko_1") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ko_2") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ko_3") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ko_4") ?? UIImage())
-        case "ja":
-            tutorialImages.append(UIImage(named: "tutorial_ja_1") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ja_2") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ja_3") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_ja_4") ?? UIImage())
-        case .none:
-            tutorialImages.append(UIImage(named: "tutorial_eng_1") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_2") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_3") ?? UIImage())
-            tutorialImages.append(UIImage(named: "tutorial_eng_4") ?? UIImage())
-            break
-        case .some(_):
-            break
+        if var code = Locale.current.languageCode {
+            if code != "ko" && code != "ja" && code != "en" { code = "en" }
+            
+            let tutorial_str = "tutorial_"
+            let tutorial_str_1 = tutorial_str + code + "_1"
+            let tutorial_str_2 = tutorial_str + code + "_2"
+            let tutorial_str_3 = tutorial_str + code + "_3"
+            let tutorial_str_4 = tutorial_str + code + "_4"
+            
+            tutorialImages.append(UIImage(named: tutorial_str_1) ?? UIImage())
+            tutorialImages.append(UIImage(named: tutorial_str_2) ?? UIImage())
+            tutorialImages.append(UIImage(named: tutorial_str_3) ?? UIImage())
+            tutorialImages.append(UIImage(named: tutorial_str_4) ?? UIImage())
         }
         
         let pageWidth = self.view.frame.width
@@ -142,8 +164,8 @@ extension TutorialViewController {
             let offset = CGFloat(index) * pageWidth
             
             backView.snp.makeConstraints { (make) in
-                make.width.equalTo(view.frame.width)
-                make.height.equalTo(view.frame.height)
+                make.width.equalToSuperview()
+                make.height.equalToSuperview()
                 make.left.equalToSuperview().offset(offset)
                 make.centerY.equalToSuperview()
             }
@@ -160,9 +182,14 @@ extension TutorialViewController {
         tutorialDoneButton.snp.makeConstraints { (make) in
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview().offset(50)
-            make.width.equalToSuperview().multipliedBy(0.4)
+//            make.width.equalToSuperview().multipliedBy(0.4)
+            make.width.equalTo(view.frame.width*0.4)
             make.height.equalTo(50)
         }
+    }
+    
+    private func setupNavigationController() {
+        navigationController?.navigationBar.isHidden = true
     }
 }
 
