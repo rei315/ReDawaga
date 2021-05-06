7e96e143c83c5a79b1e7ffdff82afa31fff04e89
Kim MinGuk <alsrnr315@gmail.com>
2021-05-06 13:02:53 +0900

    Update UIView+Extension ( Add rotation )

--- ReDawaga/Extension/UIView+Extension.swift
+++ ReDawaga/Extension/UIView+Extension.swift
@@ -15,4 +15,25 @@ extension UIView {
         mask.path = path.cgPath
         layer.mask = mask
     }
+    
+    private static let kRotationAnimationKey = "rotationanimationkey"
+    
+    func rotate(duration: Double = 1) {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) == nil {
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Float.pi * 2.0
+            rotationAnimation.duration = duration
+            rotationAnimation.repeatCount = Float.infinity
+            
+            layer.add(rotationAnimation, forKey: UIView.kRotationAnimationKey)
+        }
+    }
+    
+    func stopRotating() {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) != nil {
+            layer.removeAnimation(forKey: UIView.kRotationAnimationKey)
+        }
+    }
 }
